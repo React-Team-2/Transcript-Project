86b8a5399a67aadd03ed1e5258c701cc0e334514
Victor-Aremu <77357316+Victor-Aremu@users.noreply.github.com>
2021-06-07 13:01:15 +0000

    ProtectedRoute Fixed

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -8,11 +8,22 @@ export default function ProtectedRoute({component: Component,...rest}) {
         
         <Route  {...rest} render={
             (props)=>{
-                let token = localStorage.getItem('token') 
+                console.log(props)
+                let token = localStorage.getItem('token')
+                let user = localStorage.getItem('roleTitle')
                 console.log(token)
+                console.log(user)
+                console.log(props.match.url)
                 if(token || Auth.isAuthenticated()){
                     console.log("Access granted sucessfully")
-                return <Component exact {...props}/>
+                    if (user === 'admin' && props.match.url === '/admin'){
+                        return <Component exact {...props.match.url}/>
+                    }
+                    else if(user === 'user' && props.match.url === '/trainee'){
+                        return <Component exact {...props.match.url} />
+                    }
+                    
+                    return <Redirect to={{pathname:"/", state:{from:props.location}}}/>
                 }else{
                     console.log("You don not have acess to this page")
                    return <Redirect to={{pathname:"/", state:{from:props.location}}}/>
