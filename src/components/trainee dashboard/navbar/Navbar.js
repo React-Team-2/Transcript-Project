98ba2d587d5cfaa87cc66f73f7ba71297e176974
Victor-Aremu <victor.aremu@amalitech.org>
2021-06-08 11:43:26 +0000

    profile fixed

--- src/components/trainee dashboard/navbar/Navbar.js
+++ src/components/trainee dashboard/navbar/Navbar.js
@@ -1,7 +1,12 @@
 import "./Navbar.css";
-// import avatar from "../../../../public/assets/avatar.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Button} from 'react-bootstrap'
 
 const Navbar = ({ sidebarOpen, openSidebar }) => {
+
+  const handleLogout =()=>{
+
+  }
   return (
     <nav className="navbar">
       <div className="nav_icon" onClick={() => openSidebar()}>
@@ -13,21 +18,30 @@ const Navbar = ({ sidebarOpen, openSidebar }) => {
          Trainee
         </a>
       </div>
-      <div className="navbar__right">
-       
-       
+      {/* <div className="navbar__right">   
         <a href="#">
-          {/* <img width="30" src={avatar} alt="avatar" /> */}
+          <img width="30" src='/assets/avatar.svg' alt="avatar" />
         </a>
-      
       </div>
       <div className="dropdown">
           <ul>
             <li>VictorAremu</li>
             <li>Batch 3</li>
             <li>Media & Design</li>
-            <li><a>Log out</a></li>
+            <Button onClick={()=>{handleLogout()}}> Log out </Button>
           </ul>
+        </div> */}
+
+        <div className="dropdown mr-1">
+          <button type="button" id="dropDownButton" className="btn mr-1 dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="10,20">
+            <img width="30" src='/assets/avatar.svg' alt="avatar" />
+          </button>
+          <div className="dropdown-menu mr-5" aria-labelledby="dropDownButton">
+              <a href="#" className="dropdown-item">VictorAremu</a>
+              <a href="#" className="dropdown-item">Batch 3</a>
+              <a href="#" className="dropdown-item">Media & Design</a>
+              <a href="#" className="dropdown-item"><Button onClick={()=>{handleLogout()}}>sign out</Button></a>
+            </div>
         </div>
     </nav>
   );
