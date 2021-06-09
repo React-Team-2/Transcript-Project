048b4c8f88a3f21be8b0edf79af309ed67f7e6f0
Felix Portuphy <felix.portuphy@amalitech.org>
2021-06-09 13:23:31 +0000

    handled fetching tracks from api endpoint

--- src/components/trainee dashboard/pages/track/Track.js
+++ src/components/trainee dashboard/pages/track/Track.js
@@ -1,99 +1,130 @@
-import React, { Component } from 'react';
-import {connect} from "react-redux";
-import '../courses/Course.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "../courses/Course.css";
 import axios from "axios";
 
 class track extends Component {
-    render() {
-        async function fetchTracks(){
-            const fetchedTracks = await axios.get('https://amalitech-tms.herokuapp.com/tracks?user=60ba4ef1303e0f1b04b7a26c', {
-                headers: {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImplZmYucG9ydHVwaHkyQGFtYWxpdGVjaC5vcmciLCJ1c2VyX2lkIjoiNjBiYTRlZjEzMDNlMGYxYjA0YjdhMjZjIiwiaWF0IjoxNjIyOTA4ODEwfQ.QBB21xZtGNRWW_1F6Vb0V47kPIRIRHgOsnktxSA_vT4"}
-            })
-            return fetchedTracks.data.result;
-       }
+  constructor(props){
+    super(props);
+    this.fetchTracks();
+  }
 
-       
-       fetchTracks().then(tracks =>{
-        this.props.updatetracks(tracks)
-       })
-       
-        let trackData =this.props.track;
-        //Date Created
-        trackData.map(track=>{
-            const date = new Date(track.enrollment_date);
-            track.enrollment_date = date.toLocaleDateString();
-        })
-        return (
-            <div class="container p-5">
-            <div class="row">
-              <div class="col-10">
-                Track:<span class="ms-3">track_name</span>
-              </div>
-             
-            </div>
-            <div class="row mt-3">
-              <div class="col-10">
-                <div className="d-flex">
-                <label for="sort">Sort</label>
-                <select id="sort" class="form-select w-auto ms-3" aria-label="Default select example">
-                  <option>ASC</option>
-                  <option>DESC</option>
-                </select>
-                </div>
-              </div>
-              <div class="col">
-                <button className="btn btn-success">Print</button>
-              </div>
-            </div>
-            <hr/>
-            <table className="table table-striped table-hover">
-              <thead className="table-light">
-                <tr>
-                  <th>#</th>
-                  <th>Track Title</th>
-                  <th>Track Master</th>
-                  <th>Date Enrolled</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                 
-                {trackData.map((track,index) => {
-                  return (
-                    <tr>
-                      <td>{index}</td>
-                      <td>{track.track_name}</td>
-                      <td>{track.track_master}</td>
-                      <td>{track.enrollment_date}</td>
-                     
-                      <td><button className="btn btn-primary">View</button></td>
 
-                     
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+ 
+
+  async fetchTracks() {
+    const fetchedTracks = await axios.get(
+      "https://amalitech-tms.herokuapp.com/tracks?user=60ba4ef1303e0f1b04b7a26c",
+      {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImplZmYucG9ydHVwaHkyQGFtYWxpdGVjaC5vcmciLCJ1c2VyX2lkIjoiNjBiYTRlZjEzMDNlMGYxYjA0YjdhMjZjIiwiaWF0IjoxNjIyOTA4ODEwfQ.QBB21xZtGNRWW_1F6Vb0V47kPIRIRHgOsnktxSA_vT4",
+        },
+      }
+    );
+    const trackData = fetchedTracks.data.result;
+        
+    trackData.map((track) => {
+      const date = new Date(track.enrollment_date);
+      track.enrollment_date = date.toLocaleDateString();
+    });
+    this.props.getTracks(trackData);
+  }
+
+
+
+  render() {
+    let trackData = this.props.track;
+    let fetched = this.props.loading;
+    
+    // Date Created
+
+
+    //conditional rendering
+    const loadTracks = fetched ? (
+      <tbody>
+        {trackData.map((track, index) => {
+          return (
+            <tr key={index}>
+              <td>{index}</td>
+              <td>{track.track_name}</td>
+              <td>{track.track_master}</td>
+              <td>{track.enrollment_date}</td>
+
+              <td>
+                <button className="btn btn-primary">View</button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    ) : (
+      <tbody>
+        <tr>
+          <td className="text-center" colSpan="10">
+            <div className="spinner-border m-5" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    );
+
+    return (
+      <div class="container p-5">
+        <div class="row">
+          <div class="col-10">
+            Track:<span class="ms-3">track_name</span>
           </div>
-         
+        </div>
+        <div class="row mt-3">
+          <div class="col-10">
+            <div className="d-flex">
+              <label for="sort">Sort</label>
+              <select
+                id="sort"
+                class="form-select w-auto ms-3"
+                aria-label="Default select example"
+              >
+                <option>ASC</option>
+                <option>DESC</option>
+              </select>
+            </div>
+          </div>
+          <div class="col">
+            <button className="btn btn-success">Print</button>
+          </div>
+        </div>
+        <hr />
+        <table className="table table-striped table-hover">
+          <thead className="table-light">
+            <tr>
+              <th>#</th>
+              <th>Track Title</th>
+              <th>Track Master</th>
+              <th>Date Enrolled</th>
+              <th>Action</th>
+            </tr>
+          </thead>
 
-
-          
-        );
-    }
+          {loadTracks}
+        </table>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state =>{
-     return{
-         track:state.allTracks
-     }
-}
+const mapStateToProps = (state) => {
+  return {
+    loading: state.allTracks.fetched,
+    track: state.allTracks.tracks,
+  };
+};
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getTracks: (tracks) => dispatch({ type: "UPDATE_TRACKS", payload: tracks}),
+  };
+};
 
-const mapDispatchToProps = dispatch =>{
-    return{
-        updatetracks:(tracks)=>dispatch({type:"UPDATE_TRACKS", value: tracks})
-        }
-}
-
-export default connect(mapStateToProps,mapDispatchToProps) (track);
+export default connect(mapStateToProps, mapDispatchToProps)(track);
--- src/store/reducers/AllTracksReducer.jsx
+++ src/store/reducers/AllTracksReducer.jsx
@@ -1,23 +1,18 @@
 
-// const trackState =[
-//     {id:"1",title:"Foundation",code:"PWJ",date:"2021-01-04"},
-//     {id:"2",title:"Media & Design",code:"PWJ",date:"2021-01-04"},
-//     {id:"3",title:"Software Development & Evolution",code:"PWJ",date:"2021-01-04"},
-//     {id:"4",title:"Machine Learning & Data Analytics",code:"PWJ",date:"2021-01-04"},
-//     {id:"5",title:"Salesforce Commerce Cloud Development",code:"STWC",date:"2021-03-15"},
+const AllTracksReducer = (state={tracks:[], fetched:false}, action) => {
     
-// ]
-
-
-
-
-const AllTracksReducer = (state=[], action) => {
 
     switch(action.type){
         case "UPDATE_TRACKS":
-           return action.value
+           return {
+               tracks: action.payload,
+               fetched:true
+           }
+          
+
         default: return state
     }
+   
 }
 
 export default AllTracksReducer;
